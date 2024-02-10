cf107f11efed9e65c2d888138a9c0c7138b95100
Ali Mostafa Radwan <66019196+AliMostafaRadwan@users.noreply.github.com>
2024-02-10 20:41:03 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -3,6 +3,6 @@ TODO:
 - fix the setting interface qss not readable
 - add the arabic language (done)
 - make the first intereface save the roi to the same foramt as the previous one
-- add the undo and redo to the first interface
+- add the undo and redo to the first interface (done)
 
 """

e2e40a6b8d080dcbc34c0bba3b2d7f7f194a0808
Ali Mostafa Radwan <66019196+AliMostafaRadwan@users.noreply.github.com>
2024-02-10 21:28:56 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -5,4 +5,5 @@ TODO:
 - make the first intereface save the roi to the same foramt as the previous one
 - add the undo and redo to the first interface (done)
 
+- ignored for now is the auto detect bubbles (do later)
 """

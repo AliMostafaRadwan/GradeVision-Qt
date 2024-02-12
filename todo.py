defe6fdbee77093266ccb9d7d17a755f98c85f3d
Ali Mostafa Radwan <66019196+AliMostafaRadwan@users.noreply.github.com>
2024-02-12 22:40:35 +0200

    checked the added stuff

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@
 TODO:
 - fix the setting interface qss not readable
 - add the arabic language (done)
-- make the first intereface save the roi to the same foramt as the previous one
+- make the first intereface save the roi to the same foramt as the previous one (done)
 - add the undo and redo to the first interface (done)
 
 - ignored for now is the auto detect bubbles (do later)
